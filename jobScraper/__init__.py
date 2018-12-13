9629fd8317b25568ee30872d11315cb87d3703e0
REW <reuben.wong@p3-group.com>
2018-12-12 21:39:53 -0600

    added search functionality. Users can input job description and cities to query indeed web data

--- jobScraper/__init__.py
+++ jobScraper/__init__.py
@@ -1,28 +1,55 @@
-from flask import Flask, render_template
-import pandas as pd
+#------------------------------------------------
+###IMPORTING LIBRARIES AND CLASSES###
+#------------------------------------------------
+from flask import Flask, request, render_template #import flask class to begin web development
+import pandas as pd #import pandas to deal with dataframes
+from jobScraper import jobs #call and import jobs.py file
 
-#Test to call other python script
-#from jobScraper import Hello
-#Hello.hello()
+#------------------------------------------------
+###FLASK APPLICATION###
+#------------------------------------------------
+app = Flask(__name__) #create instance of flask class
 
-from jobScraper import jobs
-web_data = jobs.df
-
-app = Flask(__name__)
-
-@app.route('/')
+@app.route('/') #route() decorator to tell Flask what URL should trigger our function
 def index():
     return render_template('home.html')
 
-@app.route('/data')
-def data():
-    return render_template('data.html',data=web_data.to_html())
+@app.route('/', methods=['POST']) #route() for our search box
+def my_form_post(): #processing text from search box
+    text1 = request.form['job'] #get user input text for job
+    text2 = request.form['cities'] #get user input for cities to search through
 
-@app.route('/about')
+    words = text1.split() #split up user text into a list for processing
+    cities = text2.split() #split up cities into list for processing
+
+    search = "" #initialize empty string that will contain the job description to search
+    for word in words: #for loop to make job description format fit into indeed url
+        search = search + word + "+" 
+    print("we are searching for the job description: ",search)
+
+    data = jobs.job_search(search,cities)
+
+    #-----------------------------------
+    # Putting data into sqlite database
+    #-----------------------------------
+    import sqlite3
+    conn = sqlite3.connect("jobs.db") #Create connection to SQLite database
+    c = conn.cursor() #define cursor object to perform SQL commands
+
+    c.execute('DROP TABLE IF EXISTS jobs;') #drop table
+    sql = """CREATE TABLE IF NOT EXISTS jobs (ID integer, title varchar(50),company varchar(50),location varchar(30),salary varchar(20),description text);""" 
+
+    c.execute(sql)
+    data.to_sql(name='jobs',con=conn,if_exists='append',index=False)
+
+    conn.close #close connection
+    return render_template('data.html',data=data.to_html())
+
+@app.route('/about') #route for the about page
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact') #route for the contact page
 def contact():
     return render_template('contact.html')
 
--- jobScraper/jobs.py
+++ jobScraper/jobs.py
@@ -1,3 +1,6 @@
+#------------------------------------------------
+###IMPORTING LIBRARIES AND CLASSES###
+#------------------------------------------------
 import bs4
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as uReq
@@ -43,50 +46,38 @@ def get_description(div):
         return 'None'
 title=[]
 
-#Initializing empty dataframe
-ID = []
-company=[]
-location=[]
-salary=[]
-description=[]
-
-#for city in city_list: #NOTE: have to further add city into url
-
-no_results = 300 #total number of results to return for each city
-per_page = 50 #number of results to scrape per page for each city
-job = "supply+chain+analyst"
+###TEST INPUT###
+job = "project+manager" 
 cities = ["Austin","San+Francisco","Boston","Chicago","Los+Angeles","New+York","Washington"]
 
-labels = ["ID","title","company","location","salary","description"] #dataframe column labels
-df = pd.DataFrame(columns=labels) #dataframe initialization
+def job_search(job, cities):
+    #Initializing empty dataframe
+    ID = []
+    company=[]
+    location=[]
+    salary=[]
+    description=[]
 
-for city in cities: #looping through predefined list of cities
-        print('compiling results for: ',city)
-        for page in range(0,no_results,per_page): #looping through each page in increments of per_page
-            URL = "https://www.indeed.com/jobs?q={}&l={}&start={}&limit={}".format(job,city,page,per_page)
-            page = requests.get(URL) #retrieve URL
-            page_soup = soup(page.text,"html5lib") #parse URL
+    #for city in city_list: #NOTE: have to further add city into url
 
-            counter = 1
-            for div in page_soup.find_all("div",attrs={"class":"row"}): #for loop through all rows to extract results
-                df = df.append({'ID':counter,'title':get_title(div),'company':get_company(div),'location':get_location(div),
-                       'salary':get_salary(div),'description':get_description(div)},ignore_index=True) #update dataframe with job searches
-                counter = counter + 1
-            sleep(1) #sleeping one second between scrapes    
-df[['title','company']]
+    no_results = 100 #total number of results to return for each city
+    per_page = 50 #number of results to scrape per page for each city
 
-#----------------------------
-# SQLITE
-#----------------------------
-import sqlite3
-conn = sqlite3.connect("jobs.db") #Create connection to SQLite database
-c = conn.cursor() #define cursor object to perform SQL commands
+    labels = ["ID","title","company","location","salary","description"] #dataframe column labels
+    df = pd.DataFrame(columns=labels) #dataframe initialization
+    for city in cities: #looping through predefined list of cities
+            print('compiling results for: ',city)
+            for page in range(0,no_results,per_page): #looping through each page in increments of per_page
+                URL = "https://www.indeed.com/jobs?q={}&l={}&start={}&limit={}".format(job,city,page,per_page)
+                page = requests.get(URL) #retrieve URL
+                page_soup = soup(page.text,"html5lib") #parse URL
 
-c.execute('DROP TABLE IF EXISTS jobs;') #drop table
-sql = """CREATE TABLE IF NOT EXISTS jobs (ID integer, title varchar(50),company varchar(50),location varchar(30),salary varchar(20),description text);""" 
+                counter = 1
+                for div in page_soup.find_all("div",attrs={"class":"row"}): #for loop through all rows to extract results
+                    df = df.append({'ID':counter,'title':get_title(div),'company':get_company(div),'location':get_location(div),
+                           'salary':get_salary(div),'description':get_description(div)},ignore_index=True) #update dataframe with job searches
+                    counter = counter + 1
+                sleep(1) #sleeping one second between scrapes
+    return df
 
-c.execute(sql)
-df.to_sql(name='jobs',con=conn,if_exists='append',index=False)
 
-c.execute("select * from jobs")
-conn.close #close connection
